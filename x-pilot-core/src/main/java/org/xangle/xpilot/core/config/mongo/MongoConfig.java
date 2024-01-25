837031f37ebccb2a94768d91306b0348d89e3432
서명현 <merry@crossangle.io>
2024-01-25 18:21:13 +0900

    feat: _class 컬럼 제거하는 설정 추가

---
+++ x-pilot-core/src/main/java/org/xangle/xpilot/core/config/mongo/MongoConfig.java
@@ -0,0 +1,19 @@
+package org.xangle.xpilot.core.config.mongo;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+
+@Configuration
+@RequiredArgsConstructor
+public class MongoConfig {
+
+    private final MappingMongoConverter mappingMongoConverter;
+
+    @PostConstruct
+    public void setUpMongoEscapeCharacterConversion() {
+        mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+    }
+}
